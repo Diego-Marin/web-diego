b5313efe5fb3aafcbe62c5eeffb5e54de6cef3d9
Diego Mejia <diego.mejia@pascualbravo.edu.co>
2023-08-23 16:11:08 -0500

    Actualizacion a nueva interfaz del portafolio, donde la navegacion por secciones cambio, modificando todo el codigo del proyecto

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let root = document.documentElement;
 
 root.addEventListener("mousemove", (e) => {
-	root.style.setProperty("--mouse-x", e.clientX + "px");
-	root.style.setProperty("--mouse-y", e.clientY + "px");
+  root.style.setProperty("--mouse-x", e.clientX + "px");
+  root.style.setProperty("--mouse-y", e.clientY + "px");
 });
+
+// Funcion para mostrar una seccion y ocultar las demas.
+function mostrarSeccion(idSeccion) {
+  const secciones = document.getElementsByClassName('seccion');
+
+  for (let i = 0; i < secciones.length; i++) {
+    secciones[i].classList.remove('seccion-visible');
+  }
+
+  const seccionMostrar = document.getElementById(idSeccion);
+  if (seccionMostrar) {
+    seccionMostrar.classList.add('seccion-visible');
+  }
+}
